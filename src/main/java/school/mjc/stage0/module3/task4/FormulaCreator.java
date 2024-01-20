9ad96e1f1188b52d32cace90c3bce899f2d3eeb1
Rico4eT <shiwawa96@mail.ru>
2024-01-20 16:51:19 +0500

    My Primitives task

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,13 +2,20 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-       // byte nine = 9;
-       // byte two = 2;
-       // byte seven = 7;
-       // byte four = 4;
+        byte nine = 9;
+        byte two = 2;
+        byte seven = 7;
+        byte four = 4;
+        int five = 5;
+        char a = 'a';
+        char b = 'b';
+        int first_part = nine * a * a - five * b + two + a - 7;
+        int second_part = (a + b - four * a * b) / two;
+        int result = first_part * second_part;
+        //System.out.println(result);
       //  String b = "x³ + x² + 1 = y²";
       //  String a = "9a\\u00B2";//"a²";
         //System.out.println(a);
-        System.out.println("(9a\u00B2 - 5b + 2 + a - 7)*((a + b - 4ab)/2)");
+       // System.out.println("(9a\u00B2 - 5b + 2 + a - 7)*((a + b - 4ab)/2)");
     }
 }
