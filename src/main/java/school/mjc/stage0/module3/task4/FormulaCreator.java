54f3b3c75a3c17bd3adbbe14e86a1015056c5f93
Rico4eT <shiwawa96@mail.ru>
2024-01-21 09:19:48 +0500

    My Primitives task

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,11 +1,9 @@
 package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
-        public static void main (String[] args){
-            byte a = 1;
-            byte b = 2;
-            System.out.println((9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
-        }
+    public static void main (String[] args){
+        byte a = 1;
+        byte b = 2;
+        System.out.println((9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
     }
-
-
+}
